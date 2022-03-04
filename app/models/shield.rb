6cbd36ab1c9ea15ddcfbcda6691fa4e6038b9dbf
E-Vargo <vargoelliott@gmail.com>
2022-03-04 13:27:03 -0800

    new sword form slug for sword, shield

--- app/models/shield.rb
+++ app/models/shield.rb
@@ -1,3 +1,11 @@
 class Shield < ActiveRecord::Base
     belongs_to :user
+
+    def slug 
+        self.name.gsub(" ", "-").downcase
+    end
+      
+    def self.find_by_slug(slug)
+      self.all.find {|s| s.slug == slug}
+    end
 end
